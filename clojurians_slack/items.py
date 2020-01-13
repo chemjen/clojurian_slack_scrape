ea71e22cb418cc9c7960ef865824276002beb16a
Jen Ruddock <pchemjen@gmail.com>
2020-01-13 09:51:25 -0500

    add a basic items.py

--- clojurians_slack/items.py
+++ clojurians_slack/items.py
@@ -11,4 +11,7 @@ import scrapy
 class ClojuriansSlackItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    channel = scrapy.Field()
+    date = scrapy.Field()
+    user = scrapy.Field()
+    text = scrapy.Field()
